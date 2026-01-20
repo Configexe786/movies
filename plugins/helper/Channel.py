663ef73ce8898b6ab26372cec97966b9efb176c5
Configexe786 <tgmastergaming@gmail.com>
2026-01-21 02:53:06 +0530

    Update Channel.py

--- plugins/helper/Channel.py
+++ plugins/helper/Channel.py
@@ -2,19 +2,21 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import *
 
-# Replace this with your own channel ID
-CHANNEL_ID = -1001944607209
+# Tumhara Database Channel ID
+CHANNEL_ID = -1003284007507
 
 @Client.on_message(filters.channel & filters.media)
 async def add_button(client, message):
     if message.chat.id == CHANNEL_ID:
+        # Tumhara Movie Group Link
         button = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔰𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽🔰", url="https://t.me/learning_bots")]]
+            [[InlineKeyboardButton("🔰𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽🔰", url="https://t.me/NetflirtMovieGroup")]]
         )
         
         try:
-            # Try to add the button to the message
+            # Message par button add karne ke liye
             await message.edit_reply_markup(reply_markup=button)
-            await asyncio.sleep(0.5)  # Small delay to handle rapid messages
+            await asyncio.sleep(0.5)  # Rapid messages handle karne ke liye delay
         except Exception as e:
             print(f"Failed to add button: {e}")
+            
